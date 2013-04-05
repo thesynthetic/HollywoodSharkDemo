356d38e3ecdd73809b67847da6b126678cfbd196
Ryan <ryeguy_24@yahoo.com>
2013-04-05 11:59:49 -0400

    Initial Commit

---
+++ HollywoodSharkDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HollywoodSharkDemo
+//
+//  Created by Ryan Hittner on 4/5/13.
+//  Copyright (c) 2013 Ryan Hittner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HollywoodSharkDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HollywoodSharkDemo
+//
+//  Created by Ryan Hittner on 4/5/13.
+//  Copyright (c) 2013 Ryan Hittner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
